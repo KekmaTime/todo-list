26062661b486971be2c11db36bd5dcd3623a8c51
KekmaTime <22am014@sctce.ac.in>
2023-11-24 23:33:52 +0530

    Inputs and UI added

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,62 @@
 use ncurses::*;
+use std::cmp::*;
+
+const REGULAR_PAIR: i16 = 0;
+const HIGHLIGHT_PAIR: i16 = 1;
+const KEY_UP: i32 = 259;
+const KEY_DOWN: i32 = 258;
+const QUIT: i32 = 113;
 
 fn main() {
     initscr();
+    noecho();
+    keypad(stdscr(), true);
+
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+
+    start_color();
+    init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
+    init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
+
     let mut quit = false;
     let todos = vec!["Do ProjectEuler", "Do Ferrous", "Shave"];
+    let mut todo_curr: usize = 0;
 
-    for (row, todo) in todos.iter().enumerate() {
-        mvprintw(row as i32, 1, todo);
-    }
-    refresh();
-
+    refresh_todos(&todos, todo_curr);
     while !quit {
+
         let key = getch();
-        match key as u8 as char {
-            'q' => quit = true,
+        match key {
+            KEY_UP => {
+                if todo_curr > 0 {
+                    todo_curr -= 1;
+                    refresh_todos(&todos, todo_curr);
+                }
+            }
+            KEY_DOWN => {
+                if todo_curr < todos.len() - 1 {
+                    todo_curr = min(todo_curr + 1, todos.len() - 1);
+                    refresh_todos(&todos, todo_curr);
+                }
+            }
+            QUIT => quit = true,
             _ => {}
         }
     }
     endwin();
+}
+
+fn refresh_todos(todos: &[&str], todo_curr: usize) {
+    clear();
+    for (index, &todo) in todos.iter().enumerate() {
+        let pair = if index == todo_curr {
+            HIGHLIGHT_PAIR
+        } else {
+            REGULAR_PAIR
+        };
+        attron(COLOR_PAIR(pair));
+        mvprintw(index as i32, 1, todo);
+        attroff(COLOR_PAIR(pair));
+    }
+    refresh();  
 }
