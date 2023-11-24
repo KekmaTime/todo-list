12aa35cccc086b200928b60c550e3f267b2d0300
KekmaTime <22am014@sctce.ac.in>
2023-11-25 00:03:00 +0530

    Removed refresh-todo function coz it was annoying when i tried implementing the UI

--- src/main.rs
+++ src/main.rs
@@ -22,21 +22,31 @@ fn main() {
     let todos = vec!["Do ProjectEuler", "Do Ferrous", "Shave"];
     let mut todo_curr: usize = 0;
 
-    refresh_todos(&todos, todo_curr);
     while !quit {
+        clear();
+
+        for (index, &todo) in todos.iter().enumerate() {
+            let pair = if index == todo_curr {
+                HIGHLIGHT_PAIR
+            } else {
+                REGULAR_PAIR
+            };
+            attron(COLOR_PAIR(pair));
+            mvprintw(index as i32, 1, todo);
+            attroff(COLOR_PAIR(pair));
+        }
+        refresh();
 
         let key = getch();
         match key {
             KEY_UP => {
                 if todo_curr > 0 {
                     todo_curr -= 1;
-                    refresh_todos(&todos, todo_curr);
                 }
             }
             KEY_DOWN => {
                 if todo_curr < todos.len() - 1 {
                     todo_curr = min(todo_curr + 1, todos.len() - 1);
-                    refresh_todos(&todos, todo_curr);
                 }
             }
             QUIT => quit = true,
@@ -44,19 +54,4 @@ fn main() {
         }
     }
     endwin();
-}
-
-fn refresh_todos(todos: &[&str], todo_curr: usize) {
-    clear();
-    for (index, &todo) in todos.iter().enumerate() {
-        let pair = if index == todo_curr {
-            HIGHLIGHT_PAIR
-        } else {
-            REGULAR_PAIR
-        };
-        attron(COLOR_PAIR(pair));
-        mvprintw(index as i32, 1, todo);
-        attroff(COLOR_PAIR(pair));
-    }
-    refresh();  
 }
